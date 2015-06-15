be0830b82c52afe7248c7843ad68c29b3353352d
mikemix <michal@durooil.com>
2015-06-15 16:34:55 +0200

    Register annotations with doctrine

--- src/Module.php
+++ src/Module.php
@@ -1,8 +1,15 @@
 <?php
 namespace mxdiModule;
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
 class Module
 {
+    public function init()
+    {
+        AnnotationRegistry::registerAutoloadNamespace('mxdiModule\\', __DIR__ . '/../src');
+    }
+
     public function getConfig()
     {
         return (array)require __DIR__ . '/../config/module.config.php';
--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -5,6 +5,11 @@ use mxdiModule\Module;
 
 class ModuleTest extends \PHPUnit_Framework_TestCase
 {
+    public function testInitRegistersNamespaceForAnnotations()
+    {
+        (new Module())->init();
+    }
+
     public function testGetConfig()
     {
         $this->assertInternalType('array', (new Module())->getConfig());
