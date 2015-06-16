f9e92496658bc0477f1b9cb75b420b1e0f2cc7c3
mikemix <michal@durooil.com>
2015-06-16 15:39:47 +0200

    Custom autoloader for annotations

--- src/Module.php
+++ src/Module.php
@@ -7,7 +7,14 @@ class Module
 {
     public function init()
     {
-        AnnotationRegistry::registerAutoloadNamespace('mxdiModule\\', __DIR__ . '/../src');
+        AnnotationRegistry::registerLoader(function ($class) {
+            if (strpos($class, 'mxdiModule\\Annotation') !== false) {
+                require_once sprintf('%s/Annotation/%s.php', __DIR__, basename($class));
+                return true;
+            }
+
+            return false;
+        });
     }
 
     public function getConfig()
