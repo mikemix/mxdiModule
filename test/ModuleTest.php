2d1b7c526a85d0330a7a49c657de3618f17881d1
mikemix <michal@durooil.com>
2015-06-18 13:09:04 +0200

    Module test update

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -2,24 +2,53 @@
 namespace mxdiModuleTest;
 
 use mxdiModule\Module;
+use Zend\Console\Adapter\AdapterInterface;
 
 class ModuleTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var Module */
+    private $module;
+
+    public function setUp()
+    {
+        $this->module = new Module();
+    }
+
     public function testInitCallable()
     {
-        (new Module())->init();
+        $this->module->init();
     }
 
     public function testGetConfig()
     {
-        $this->assertInternalType('array', (new Module())->getConfig());
+        $this->assertInternalType('array', $this->module->getConfig());
     }
 
     public function testAutoloader()
     {
-        $module = new Module();
+        $this->assertFalse($this->module->annotationAutoloader('mxdiModule\Service\ChangeSet'));
+        $this->assertTrue($this->module->annotationAutoloader('mxdiModule\Annotation\Inject'));
+    }
 
-        $this->assertFalse($module->annotationAutoloader('mxdiModule\Service\ChangeSet'));
-        $this->assertTrue($module->annotationAutoloader('mxdiModule\Annotation\Inject'));
+    public function testGetBanner()
+    {
+        /** @var AdapterInterface $adapter */
+        $adapter = $this->getMockBuilder(AdapterInterface::class)->getMockForAbstractClass();
+
+        $banner = $this->module->getConsoleBanner($adapter);
+
+        $this->assertNotEmpty($banner);
+        $this->assertInternalType('string', $banner);
+    }
+
+    public function testGetUsage()
+    {
+        /** @var AdapterInterface $adapter */
+        $adapter = $this->getMockBuilder(AdapterInterface::class)->getMockForAbstractClass();
+
+        $usage = $this->module->getConsoleUsage($adapter);
+
+        $this->assertNotEmpty($usage);
+        $this->assertInternalType('array', $usage);
     }
 }
