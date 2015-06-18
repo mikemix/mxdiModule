526a3920dbc18f9e96c14afa5fedfa421d197aea
mikemix <michal@durooil.com>
2015-06-18 14:39:34 +0200

    Test update

--- test/Annotation/InjectLazyTest.php
+++ test/Annotation/InjectLazyTest.php
@@ -5,6 +5,7 @@ use mxdiModule\Annotation\InjectLazy;
 use mxdiModule\Factory\ProxyFactory;
 use mxdiModuleTest\TestCase;
 use ProxyManager\Factory\LazyLoadingValueHolderFactory;
+use ProxyManager\Proxy\LazyLoadingInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 class InjectLazyTest extends TestCase
@@ -34,5 +35,101 @@ class InjectLazyTest extends TestCase
             ->will($this->returnValue($factory));
 
         $this->assertSame($factory, $this->inject->getFactory($this->sm));
+    }
+
+    public function testGetValueWithMissingFQCN()
+    {
+        $factory = $this->getMock(LazyLoadingValueHolderFactory::class, ['createProxy']);
+
+        $factory->expects($this->once())
+            ->method('createProxy')
+            ->with($this->equalTo('serviceName'));
+
+        $this->sm->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo(ProxyFactory::class))
+            ->will($this->returnValue($factory));
+
+        $this->inject->value = 'serviceName';
+        $this->inject->getValue($this->sm);
+    }
+
+    public function testGetValueWithFQCN()
+    {
+        $factory = $this->getMock(LazyLoadingValueHolderFactory::class, ['createProxy']);
+
+        $factory->expects($this->once())
+            ->method('createProxy')
+            ->with($this->equalTo('fqcn'));
+
+        $this->sm->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo(ProxyFactory::class))
+            ->will($this->returnValue($factory));
+
+        $this->inject->value = 'serviceName';
+        $this->inject->fqcn = 'fqcn';
+        $this->inject->getValue($this->sm);
+    }
+
+    public function testGetValueReturnsFalseFromClosureOnServiceNotCreated()
+    {
+        $this->sm->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('serviceName'))
+            ->will($this->throwException(new \Exception()));
+
+        /** @var LazyLoadingValueHolderFactory|\PHPUnit_Framework_MockObject_MockObject $factory */
+        $factory = $this->getMock(LazyLoadingValueHolderFactory::class, ['createProxy']);
+        $factory->expects($this->once())
+            ->method('createProxy')
+            ->will($this->returnArgument(1));
+
+        /** @var LazyLoadingInterface|\PHPUnit_Framework_MockObject_MockObject $proxy */
+        $proxy = $this->getMockBuilder(LazyLoadingInterface::class)
+            ->setMethods(['setProxyInitializer'])
+            ->getMockForAbstractClass();
+
+        $this->inject->value = 'serviceName';
+        $this->inject->fqcn = 'fqcn';
+        $this->inject->setFactory($factory);
+
+        $object = new \stdClass();
+
+        /** @var \Closure $initializer */
+        $initializer = $this->inject->getValue($this->sm);
+        $this->assertFalse($initializer($object, $proxy));
+    }
+
+    public function testGetValueReturnsTrueFromClosureAndDisablesInitializer()
+    {
+        $this->sm->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('serviceName'));
+
+        /** @var LazyLoadingValueHolderFactory|\PHPUnit_Framework_MockObject_MockObject $factory */
+        $factory = $this->getMock(LazyLoadingValueHolderFactory::class, ['createProxy']);
+        $factory->expects($this->once())
+            ->method('createProxy')
+            ->will($this->returnArgument(1));
+
+        /** @var LazyLoadingInterface|\PHPUnit_Framework_MockObject_MockObject $proxy */
+        $proxy = $this->getMockBuilder(LazyLoadingInterface::class)
+            ->setMethods(['setProxyInitializer'])
+            ->getMockForAbstractClass();
+
+        $proxy->expects($this->once())
+            ->method('setProxyInitializer')
+            ->with($this->equalTo(null));
+
+        $this->inject->value = 'serviceName';
+        $this->inject->fqcn = 'fqcn';
+        $this->inject->setFactory($factory);
+
+        $object = new \stdClass();
+
+        /** @var \Closure $initializer */
+        $initializer = $this->inject->getValue($this->sm);
+        $this->assertTrue($initializer($object, $proxy));
     }
 }
